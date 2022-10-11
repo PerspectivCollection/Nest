57eae45081726fd90c46e7c1db7f61083105947d
sivert <sivert.ostgard@hiof.no>
2022-10-11 14:46:22 +0200

    fix: navigation

--- app/src/main/java/com/example/nest/navigationdraw/MainActivity.kt
+++ app/src/main/java/com/example/nest/navigationdraw/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_guesswho
+                R.id.nav_guesswho, R.id.nav_gallery, R.id.nav_namequiz
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
