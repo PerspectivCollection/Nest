decbac22d17bf5fc7494411846b3876bc2bb790d
sivert <sivert.ostgard@hiof.no>
2022-10-11 14:46:22 +0200

    fix: navigation

---
+++ app/src/main/java/com/example/nest/model/Bird.kt
@@ -0,0 +1,21 @@
+package com.example.nest.model
+
+//import com.example.nest.R
+
+
+data class Bird(val id: Int, val typeBird: String,  val name: String, val name_latin: String, val image: Int) {
+
+    companion object {
+        fun getBird(): List<Bird> {
+
+            return listOf(
+//                Bird(1, "Abacot Ranger", "latiLorem", R.drawable.streicherenten)
+
+            )
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/nest/navigationdraw/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.nest.navigationdraw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.nest.R
+import com.example.nest.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_guesswho, R.id.nav_gallery, R.id.nav_namequiz
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
