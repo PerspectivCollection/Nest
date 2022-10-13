06ed3571d62d1500cd5e2cb8fff5b08407b6d782
sivert <sivert.ostgard@hiof.no>
2022-10-13 13:42:26 +0200

    fix: guess who progresbar

--- app/src/main/java/com/example/nest/navigationdraw/GuesswhoFragment.kt
+++ app/src/main/java/com/example/nest/navigationdraw/GuesswhoFragment.kt
@@ -1,111 +1,124 @@
 package com.example.nest.navigationdraw
 
 //model
-
-//app
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import com.example.nest.R
-import com.example.nest.databinding.FragmentGuesswhoBinding
 import com.example.nest.model.Bird
+import java.time.Duration
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 
 class GuesswhoFragment : Fragment() {
-    private var _binding: FragmentGuesswhoBinding? = null
-    private val binding get() = _binding!!
-    private val viewModel: GuesswhoViewModel by viewModels()
 
-//todo set index for witch birds that have bin used. pick cird bast on the last random added
+//    private var _binding: FragmentSlideshowBinding? = null
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+//    private val binding get() = _binding!!
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        viewModel.lsitsetused.add(10)
-//    }
+
+    //start at index 0 in bird list
+    private var atNumberIndex = 0
+
+
+    fun guessFinich() {
+        if (atNumberIndex == 4) {
+            Toast.makeText(context, "${atNumberIndex}  ferdig", Toast.LENGTH_SHORT).show()
+            atNumberIndex = 0
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-
-
-        _binding = FragmentGuesswhoBinding.inflate(inflater, container, false)
-        //data bindet , can now set variabls
-        binding.sendBirdData = viewModel
-        binding.lifecycleOwner = this
-
-//        var some = viewModel.lsitsetused
-
-//        var args = GuesswhoFragmentArgs.fromBundle(requireArguments())
-//        viewModel.removeIndexFalse = args.removeIndexFalse
-////        Toast.makeText(context,  Bird.getBird().toString(), Toast.LENGTH_LONG).show()
+    ): View? {
+        /*
+//        val slideshowViewModel =
+//            ViewModelProvider(this).get(SlideshowViewModel::class.java)
 //
-//        if (viewModel.removeIndexFalse) {
-//            viewModel.whenNext()
-//        }
-
-//        Toast.makeText(context, "${viewModel.lsitsetused.toString()}", Toast.LENGTH_LONG).show()
-//        Toast.makeText(context, "${}", Toast.LENGTH_LONG).show()
-
-
-//        Toast.makeText(context, "${viewModel.random.toString()} 0  ferdig", Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "${Bird.getBird().get(0).name}", Toast.LENGTH_SHORT).show()
-
-        //button Duck
-        binding.btnStartId.setOnClickListener() {
-            navigateNext()
-//            binding.progressBar.progress += 20
-
-//            viewModel.whenNext()
-//            Toast.makeText(context, "${viewModel.indexBird}", Toast.LENGTH_SHORT).show()
-//            Bird.getBird()
-//                .get(viewModel.indexBird).image?.let { binding.imagegoosId.setImageResource(it) }
-
-
-//            val action = GuesswhoFragmentDirections.actionNavGuesswhoToFactFragment()
-//            action.typeBird =
-//                "Duck".equals(Bird.getBird().get(atNumberIndex).typeBird.toString()) //is statment
-//            action.textbird = "${Bird.getBird().get(viewModel.indexBird).name.toString()}"
+//        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
 //
-//            NavHostFragment.findNavController(this).navigate(action)
+//        val textView: TextView = binding.textSlideshow
+//        slideshowViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        } */
 
+
+        val View = inflater.inflate(R.layout.fragment_guesswho, container, false) //root
+        var Image = View.findViewById<ImageView>(R.id.imagegoosId)
+
+
+        //Duck
+        val Button1 = View.findViewById<Button>(R.id.btnOption1id)
+
+        val Button2 = View.findViewById<Button>(R.id.btnOption2id)
+
+        var process = View.findViewById<ProgressBar>(R.id.progressBar2)
+
+
+//        var TextNamBird = View.findViewById<TextView>(R.id.GetNamBirdId)
+
+
+        Button1.text = "${Bird.getBird().get(atNumberIndex).typeBird}"
+
+
+        Button1.setOnClickListener() {
+//            Toast.makeText(context, "${(Bird.getBird().get(1).typeBird)}ds } ", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(context, "${(Bird.getBird().get(1))} ", Toast.LENGTH_SHORT).show()
+
+            if ("Duck".equals(Bird.getBird().get(atNumberIndex).typeBird)) // return true add +1 to sum
+                Toast.makeText(context, "${Button1.text}  tru + 1", Toast.LENGTH_SHORT).show()
+
+
+            //Todo: put this string in to the next fragment and reapit the prosses
+//            TextNamBird.text = Bird.getBird().get(atNumberIndex).name.toString()
+
+
+                Toast.makeText(context, "${atNumberIndex}", Toast.LENGTH_SHORT).show()
+
+            // at numberIndex+ 1
+            atNumberIndex++
+
+            // a list with 5 question
+            process.progress += 20
+
+
+            //change image
+            Bird.getBird().get(atNumberIndex).image?.let { Image.setImageResource(it) }
+
+
+//            findNavController().navigate(R.id.action_)
+
+//            findNavController().navigate(R.id.fragment_fact)
+
+
+
+            guessFinich()
+        }
+
+        Button2.setOnClickListener() {
 
         }
 
 
-        return binding.root
+        return View
     }
-
-    fun navigateNext() {
-        var action = GuesswhoFragmentDirections.actionNavGuesswhoToGuessWhoStartFragment()
-
-          NavHostFragment.findNavController(this).navigate(action)
-//          findNavController().navigate(R.id.action_fact_Fragment_to_guessWhoStartFragment)
-    }
-
-    private fun guessFinich() {
-////        if (atNumberIndex == 4) {
-////            Toast.makeText(context, "${atNumberIndex}  ferdig", Toast.LENGTH_SHORT).show()
-////            atNumberIndex = 0
-////        }
-////        atNumberIndex++
-    }
-
 
 //    override fun onDestroyView() {
 //        super.onDestroyView()
 //        _binding = null
 //    }
-
 }
