8dff27e97c1ce62b1340dd8cab31cde6e421d3dc
sivert <sivert.ostgard@hiof.no>
2022-11-20 22:21:59 +0100

    fix: galleryfragment

--- app/src/main/java/com/example/nest/navigationdraw/GalleryFragment.kt
+++ app/src/main/java/com/example/nest/navigationdraw/GalleryFragment.kt
@@ -5,21 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nest.R
 import com.example.nest.adapter.GalleryAdapter
-import com.example.nest.model.GalleryStorage
-
+import com.example.nest.model.Bird
 
 class GalleryFragment : Fragment() {
-    private val movieList: List<GalleryStorage> = GalleryStorage.getGalery()
-//    private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-//    private val binding get() = _binding!!
+    private val birdCollection : List<Bird> = Bird.getBird()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,19 +25,22 @@ class GalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recycler = view.findViewById<RecyclerView>(R.id.birdRecyclerView)
 
+        recycler?.layoutManager = LinearLayoutManager(activity)
+
+//         This will pass the ArrayList to our Adapter
+
+//        val data = ArrayList<Bird>()
+//
+//        for (i in 1..20) {
+//            data.add(Bird.getBird()[i].image)
+//        }
+
+        val adapter = GalleryAdapter(birdCollection)
+
+        recycler.layoutManager = GridLayoutManager(context, 2)
+
+        recycler.adapter = adapter
     }
-        // Sets the layoutmanager we want to use
-//        movieRecyclerView.layoutManager = GridLayoutManager(context, 3)
-        //movieRecyclerView.layoutManager = GridLayoutManager(context, 2)
-        //movieRecyclerView.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.HORIZONTAL, false)
-        //movieRecyclerView.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
-        //movieRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        //movieRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-//    }
-
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
 }
