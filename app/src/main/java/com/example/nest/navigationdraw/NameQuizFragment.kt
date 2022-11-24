74b06554e434f8f170253e75615b2072582dd517
sivert <sivert.ostgard@hiof.no>
2022-11-24 20:50:33 +0100

    fix: update code

--- app/src/main/java/com/example/nest/navigationdraw/NameQuizFragment.kt
+++ app/src/main/java/com/example/nest/navigationdraw/NameQuizFragment.kt
@@ -1,6 +1,9 @@
 package com.example.nest.navigationdraw
 
 
+//model
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,21 +21,25 @@ class NameQuizFragment : Fragment() {
 
     private lateinit var questionList: ArrayList<Question>
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
 
 
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_namequiz,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_namequiz, container, false)
+
         binding.btnPlay.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_nav_namequiz_to_qustionQuizGame)
+        }
 
+        binding.shareBtn.setOnClickListener() {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"))
+            startActivity(intent)
         }
 
         return binding.root
-
-
     }
 
 }
